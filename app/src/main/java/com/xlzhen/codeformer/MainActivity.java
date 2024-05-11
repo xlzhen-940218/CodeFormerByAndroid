a6d9419149bebba84a446f1d210adaef0128bc71
xlzhen <xionglongzhen@gmail.com>
2024-05-11 10:33:25 +0800

    upload codeformer code

---
+++ app/src/main/java/com/xlzhen/codeformer/MainActivity.java
@@ -0,0 +1,149 @@
+package com.xlzhen.codeformer;
+
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.pytorch.IValue;
+import org.pytorch.LiteModuleLoader;
+import org.pytorch.MemoryFormat;
+import org.pytorch.Module;
+import org.pytorch.Tensor;
+import org.pytorch.torchvision.TensorImageUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.FileChannel;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+        File file = new File(getFilesDir().getAbsolutePath() + "/codeformer.ptl");
+        if (!file.exists()) {
+            Log.v("codeformer model", "not exist");
+            copyAssets();
+        }
+        Module module = LiteModuleLoader.load(getFilesDir().getAbsolutePath() + "/codeformer.ptl");
+
+        Log.v("module", module.toString());
+        try {
+            for (String name : Objects.requireNonNull(getAssets().list("cropped_faces"))) {
+                Bitmap bitmap = BitmapFactory.decodeStream(getAssets().open("cropped_faces/"+name));
+
+                Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
+
+                Log.v("inputTensor", inputTensor.toString());
+                IValue value = module.forward(IValue.from(inputTensor));
+                IValue[] tensors = value.toTuple();
+                Log.v("value", value.toString());
+                float[] array1 = tensors[0].toTensor().getDataAsFloatArray();
+
+                Bitmap outBitmap = bitmapFromRGBImageAsFloatArray(array1, 512, 512);
+                Log.v("output", outBitmap.toString());
+                saveBitmapToFile(outBitmap,name);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void saveBitmapToFile(Bitmap bitmap,String name) throws IOException {
+        File f = new File(Objects.requireNonNull(getExternalCacheDir()).getAbsolutePath() + "/"+name);
+        f.createNewFile();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0 /*ignored for PNG*/, bos);
+        byte[] bitmapdata = bos.toByteArray();
+
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(bitmapdata);
+        fos.flush();
+        fos.close();
+    }
+
+    public static Bitmap bitmapFromRGBImageAsFloatArray(float[] data, int width, int height) {
+
+        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        float max = 0;
+        float min = 999999;
+
+        for (float f : data) {
+            if (f > max) {
+                max = f;
+            }
+            if (f < min) {
+                min = f;
+            }
+        }
+
+        int delta = (int) (max - min);
+
+        for (int i = 0; i < width * height; i++) {
+
+
+            int r = (int) ((data[i] - min) / delta * 255.0f);
+            int g = (int) ((data[i + width * height] - min) / delta * 255.0f);
+            int b = (int) ((data[i + width * height * 2] - min) / delta * 255.0f);
+
+            int x = i / width;
+            int y = i % width;
+
+            int color = Color.rgb(r, g, b);
+            bmp.setPixel(x, y, color);
+
+        }
+        return bmp;
+    }
+
+    private void copyAssets() {
+        AssetManager assetManager = getAssets();
+
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = assetManager.open("codeformer.ptl");
+            File outFile = new File(getFilesDir().getAbsolutePath() + "/", "codeformer.ptl");
+            out = new FileOutputStream(outFile);
+            copyFile(in, out);
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+        } catch (IOException e) {
+            Log.e("tag", "Failed to copy asset file: codeformer.ptl", e);
+        }
+
+    }
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[8192];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+    }
+}
